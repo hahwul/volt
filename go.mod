0ddac3b5111a403a83cd733c2f12d4c3e49b6146
hahwul <hahwul@gmail.com>
2021-07-21 01:13:30 +0900

    git add jwt, change crypto -> token

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hahwul/volt
 
 go 1.16
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
---
+++ token/jwt.go
@@ -0,0 +1,54 @@
+package token
+
+import (
+	"github.com/dgrijalva/jwt-go"
+)
+
+// JWTencode is JWT interface for encode
+func JWTencode(claims map[string]interface{}, secret, alg string) string {
+	var key = []byte(secret)
+	var jwtClaims = jwt.MapClaims(claims)
+
+	algorithm := jwt.GetSigningMethod(alg)
+
+	token := jwt.NewWithClaims(algorithm, jwtClaims)
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString(key)
+	if err != nil {
+		return ""
+	}
+	return tokenString
+}
+
+// JWTdecode is JWT interface for decode
+func JWTdecode(tokenString string) *jwt.Token {
+	// signature := false
+
+	// Parse the token
+	var token *jwt.Token
+	var err error
+	parser := new(jwt.Parser)
+	// Figure out correct claims type
+	token, _, err = parser.ParseUnverified(tokenString, jwt.MapClaims{})
+	//token, _, err = parser.ParseUnverified(tokenString, &jwt.StandardClaims{})
+
+	if err != nil {
+		return nil
+	}
+	return token
+}
+
+// JWTdecodeWithVerify is interface for decode with verify
+func JWTdecodeWithVerify(tokenString, secret string) (bool, *jwt.Token) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		//Make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return false, nil
+		}
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return false, nil
+	}
+	return true, token
+}
